d6c71df1737c4f4102dd570682642ef84925c3a3
tdog <161662619+tdogpog@users.noreply.github.com>
2024-12-20 18:11:56 -0600

    Continued updates to admin permissions

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -6,7 +6,6 @@ async function getAllPostsAdmin(req, res) {
   try {
     const posts = await prisma.post.findMany({
       orderBy: { createdAt: "desc" },
-      where: { published: true },
       select: {
         id: true,
         title: true,
@@ -22,19 +21,48 @@ async function getAllPostsAdmin(req, res) {
 }
 async function createPost(req, res) {
   try {
-  } catch (error) {}
+    const { title, content, adminID } = req.body;
+    await prisma.post.create({
+      data: {
+        title,
+        content,
+        adminID,
+      },
+    });
+  } catch (error) {
+    console.error("Error creating post:", error.message);
+    res.status(500).json({ error: "An error occurred while creating post" });
+  }
 }
 async function updatePost(req, res) {
   try {
-  } catch (error) {}
+    const postID = req.params.postID;
+
+    await prisma.post.update({
+      where: { id: postID },
+      data: req.body,
+    });
+    res.status(200).json({ message: "Post updated" });
+  } catch (error) {
+    console.error("Error updating post:", error.message);
+    res.status(500).json({ error: "An error occurred while updating post" });
+  }
 }
 async function deletePost(req, res) {
   try {
-  } catch (error) {}
+    await prisma.post.delete({});
+  } catch (error) {
+    console.error("Error deleting post:", error.message);
+    res.status(500).json({ error: "An error occurred while deleting post" });
+  }
 }
 async function deleteComment(req, res) {
   try {
-  } catch (error) {}
+    await prisma.comment.delete({});
+  } catch (error) {
+    console.error("Error deleting comment:", error.message);
+    res.status(500).json({ error: "An error occurred while deleting comment" });
+  }
 }
 
 modules.export = {
