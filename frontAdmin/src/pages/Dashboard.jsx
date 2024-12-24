19880f6c2994628cbfb8c09b43e21560ac97de53
tdog <161662619+tdogpog@users.noreply.github.com>
2024-12-23 19:06:16 -0600

    Endpoint access standardized, Dashboard built.
    Added standardized fetch for all endpoint
    communication in api.js
    
    Built out Dashboard intended to fetch the post
    list from the backend and provide links
    to each post

--- frontAdmin/src/pages/Dashboard.jsx
+++ frontAdmin/src/pages/Dashboard.jsx
@@ -1,1 +1,50 @@
-export default function Dashboard() {}
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { authRequest } from "../api";
+
+export default function Dashboard({ backend }) {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState(null);
+  const url = `${backend}admin/posts`;
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const response = await authRequest(url);
+        if (!response.ok) {
+          throw new Error("Failed to fetch posts");
+        }
+        const data = await response.json();
+        setPosts(data);
+      } catch (err) {
+        setError(err.message);
+        console.log(err.message);
+      }
+    };
+    fetchPosts();
+  }, [url]);
+
+  if (error) {
+    return <div>Error occured: {error}</div>;
+  }
+
+  function Posts() {
+    return (
+      <ul className="posContainer">
+        {posts.map((post) => (
+          <li key={post.id} className="post">
+            <Link to={`/dashboard/${post.id}`}>{post.title}</Link>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+  return (
+    <div className="card">
+      <div className="content">
+        <h2>Posts</h2>
+        {posts.length > 0 ? <Posts /> : "No posts found"}
+      </div>
+    </div>
+  );
+}
