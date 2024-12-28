715db3f43567d1b460b029897dc193a0eff75b67
tdog <161662619+tdogpog@users.noreply.github.com>
2024-12-27 23:12:16 -0600

    Fixed outdated propTypes

--- frontAdmin/src/components/Posts.jsx
+++ frontAdmin/src/components/Posts.jsx
@@ -12,28 +12,12 @@ export default function Posts({ posts }) {
     </ul>
   );
 }
-
 Posts.propTypes = {
   posts: PropTypes.arrayOf(
     PropTypes.shape({
       id: PropTypes.string.isRequired, // uuid string
       title: PropTypes.string.isRequired,
-      content: PropTypes.string.isRequired,
-      adminID: PropTypes.string.isRequired,
       createdAt: PropTypes.string.isRequired, // ISO date string
-      published: PropTypes.bool.isRequired,
-      comments: PropTypes.arrayOf(
-        PropTypes.shape({
-          id: PropTypes.string.isRequired, // comment ID
-          content: PropTypes.string.isRequired, // comment content
-          createdAt: PropTypes.string.isRequired, // ISO date string
-          postID: PropTypes.string.isRequired, // the post to which the comment belongs
-        })
-      ),
-      admin: PropTypes.shape({
-        id: PropTypes.string.isRequired, // admin ID (uuid)
-        username: PropTypes.string.isRequired, // admin username
-      }).isRequired,
     })
   ).isRequired,
 };
